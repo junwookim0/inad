110aeedf451bd5cb5ebe8cc308f9f23f8907b8c0
junwookim <ekdlskalrx98@naver.com>
2022-05-20 16:59:03 +0900

    redeucer.jsx

--- react/hook/src/App.js
+++ react/hook/src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
 import HookComp from './component/HookComp';
+import UseReducerComp from './component/ReduserComp';
+import ReducerComp2 from './component/ReduserComp2';
+import ExReducer from './component/ExReducer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HookComp/>
-
+        <ExReducer/>
       </header>
+      <header className="App-header">
+        <HookComp/>
+      </header>
+      <header className="App-header">
+        <UseReducerComp/>
+        <ReducerComp2/>
+      </header>
+      
     </div>
   );
 }
